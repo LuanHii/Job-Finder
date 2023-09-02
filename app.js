b34728446b1b01495a70f36b45bd728443314fb1
LuanHii <luanhiivitor@gmail.com>
2023-09-02 18:43:07 -0300

    Sqlite integration, DB Created

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const db = require("./db/connection");
+
+const PORT = 3000;
+
+
+
+app.listen(PORT, function() {
+    console.log("Rodando na porta: " + PORT);
+});
+
+
+// db connection
+db
+    .authenticate()
+    .then(() => {
+        console.log("Conectado");
+    })
+    .catch(err => {
+        console.log("Erro.", err);
+    })
+
+
+// routes
+app.get("/", function(req, res) {
+    res.send("Funcionando. 3");
+})
