b4c132bbe733ed10caecb5e81ac56a515b3fa947
LuanHii <luanhiivitor@gmail.com>
2023-09-05 13:40:55 -0300

    DB updated

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express');
 const app = express();
 const db = require("./db/connection");
 const PORT = 3000;
+const bodyParser = require('body-parser');
+
+
 
 
 // starting app
 app.listen(PORT, function() {
     console.log("Rodando na porta: " + PORT);
 });
+
+
+//body parser
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 // db connection
@@ -24,4 +31,7 @@ db
 // routes
 app.get("/", function(req, res) {
     res.send("Funcionando. 3");
-})
+});
+
+//jobs routes
+app.use("/jobs", require("./routes/jobs"));
